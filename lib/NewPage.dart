0b7d30e60ffc1a8be3aafebbcb1230d9b875bb11
sushi <codingzx@gmail.com>
2019-11-07 21:34:23 +0800

    feat: init project

---
+++ lib/NewPage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+class NewRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Center(child: Text('this is new page'),),
+    );
+  }
+}
